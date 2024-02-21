3f8ae776464760a4340e5a2290dde1d3de293a96
Avinash174 <avinashmagar15@gmail.com>
2024-02-21 09:29:19 +0530

    new changes done

---
+++ lib/screens/onbording.screen/quality_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class QualityScreen extends StatefulWidget {
+  const QualityScreen({super.key});
+
+  @override
+  State<QualityScreen> createState() => _QualityScreenState();
+}
+
+class _QualityScreenState extends State<QualityScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
